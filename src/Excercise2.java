3da04bc763758e13f65fd529fba63a2717ecedba
vangtt <giangthanhvan12@gmail.com>
2023-03-01 07:38:06 +0700

    Onclass

---
+++ src/Excercise2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Excercise2 {
+    public static void main(String[] args) {
+        int rate = 23000;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input USD");
+        float dollar, vietnamdong;
+        dollar = scanner.nextFloat();
+        vietnamdong = dollar*rate;
+        System.out.println("VND is " + vietnamdong + "VND");
+    }
+}
